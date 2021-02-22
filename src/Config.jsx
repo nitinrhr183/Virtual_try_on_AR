c588d776672f5ede6a43c2519add776128dfaab2
Nitin Rahim Hameed. Rawther <nitin.rahim@litmus7.com>
2021-02-22 16:24:42 +0530

    Config changed

--- src/Config.jsx
+++ src/Config.jsx
@@ -18,7 +18,27 @@ const Images = [
         scaling: [17, 17, 17],
       },
       {
+        image: "necklace-shade.png",
+        scaling: [17, 17, 17],
+      },
+      {
         image: "1.png",
+        scaling: [17, 17, 17],
+      },
+      {
+        image: "2.png",
+        scaling: [17, 17, 17],
+      },
+      {
+        image: "3.png",
+        scaling: [17, 17, 17],
+      },
+      {
+        image: "4.png",
+        scaling: [17, 17, 17],
+      },
+      {
+        image: "5.png",
         scaling: [17, 17, 17],
       },
     ],
